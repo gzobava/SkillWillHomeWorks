f320c90fd574b9fa2b4c74a42bf82fd552db74f1
gzobava <138595912+gzobava@users.noreply.github.com>
2023-10-09 21:15:10 +0400

    dav2

--- 2/dav3/main.js
+++ 2/dav3/main.js
@@ -1,30 +1,31 @@
 function rollDice() {
-    return Math.floor(Math.random() * 6) + 1;
-  }
-  
-  function playGame() {
-    let rollsA = 0;
-    let rollsB = 0;
-  
-    while (true) {
-      const resultA = rollDice();
-      const resultB = rollDice();
-  
-      rollsA++;
-      rollsB++;
-  
-      if (resultA === 3 || resultB === 3) {
-        break;
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function playGame() {
+  let playerACount = 0;
+  let playerBCount = 0;
+
+  while (true) {
+      const result = rollDice();
+
+      if (result === 3) {
+          if (playerACount < playerBCount) {
+              console.log("გაიმარჯვა მოთამაშემ ა !");
+          } else if (playerBCount < playerACount) {
+              console.log("გაიმარჯვა მოთამაშემ ბ !");
+          } else {
+              console.log("ფრე!");
+          }
+          break;
       }
-    }
-  
-    if (resultA === 3 && resultB === 3) {
-      console.log(" ფრეე !!! ორივე მოთამაშემ თანაბარ ცდაში გააგორეს 3 ");
-    } else if (resultA === 3) {
-      console.log(`გამარჯვებულია მოთამაშე ა ! მას დასჭირდა ${rollsA} გაგორება , როდესაც მოთამაშე ბ დასჭირდა ${rollsB} გაგორება.`);
-    } else {
-      console.log(`გამარჯვებულია მოთამაშე ბ ! მას დასჭირდა ${rollsB} გაგორება , როდესაც მოთამაშე ა დასჭირდა ${rollsA} გაგორება.`);
-    }
+
+      if (Math.random() < 0.5) {
+          playerACount++;
+      } else {
+          playerBCount++;
+      }
   }
-  
-  playGame();
+}
+
+playGame();
